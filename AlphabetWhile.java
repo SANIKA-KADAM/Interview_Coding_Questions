a500ff553f4a1cd8731abf3698532c51d01a1e84
SANIKA-KADAM <sanikakadam2020@gmail.com>
2025-09-19 22:28:48 +0530

    AlphabetWhile

---
+++ AlphabetWhile.java
@@ -0,0 +1,13 @@
+public class AlphabetWhile {
+    public static void main(String[] args){
+        char ch ='a';
+
+        System.out.println("Alphabets from a to z:");
+
+        while(ch <='z'){
+            System.out.println(ch +"");
+            ch++;      
+      }
+    }
+}
+
